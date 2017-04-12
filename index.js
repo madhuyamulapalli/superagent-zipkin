990fff354e164742ca290c95cb1fd3d751577df4
Madhu Yamulapalli <madhumurty@gmail.com>
2017-04-12 19:38:37 +0530

    supaeragent-zipkin

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ if (!zipkinUrl || zipkinUrl.length > 0 && zipkinUrl.trim() === "") {
       tracer.recordRpc(method.toUpperCase());
       tracer.recordBinary("http.url", wrappedOptions.uri || wrappedOptions.url);
       tracer.recordAnnotation(new Annotation.ClientSend());
-      tracer.recordAnnotation(new Annotation.LocalAddr());
+      //tracer.recordAnnotation(new Annotation.LocalAddr());
       if (remoteServiceName) {
         tracer.recordAnnotation(new Annotation.ServerAddr({
           serviceName: remoteServiceName
